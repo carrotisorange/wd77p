19e578503fa485cf0c995ccc133b4c9f5c0072b1
carrotisorange <thepropertymanager2020@gmail.com>
2023-08-11 20:28:06 +0800

    added the code in control structures activity

---
+++ control_structure_activity/app.js
@@ -0,0 +1,55 @@
+// const NUMBER = 10;
+// const RANGE = 10;
+// let i= 1;
+
+// while(i<=RANGE) {
+//     const result = i * 10;
+//     console.log(`${NUMBER} * ${i} = ${result}`);
+//     i++;
+// }
+
+//  let n = 10; 
+ 
+//     for(i=1; i<=n; i++)
+//     {  
+//         //Prints the pyramid pattern
+//         for(j=1; j<=(2*i-1); j++)
+//         {
+//             document.write('*');
+//         }
+ 
+//         document.write('<br>');
+
+//     }
+
+// let sentence =  'this website is aw3som3';
+
+// const VOWELS = ['a','e','i','o','u'];
+// const DIGITS = ['0','1','2','3','4', '5', '6', '7', '8', '9'];
+// let vowel = 0;
+// let consonant = 0;
+// let digit = 0;
+// let space = 0;
+  
+//   for(let char of sentence){
+//     char  = char.toLowerCase();
+
+//     if(VOWELS.includes(char)){
+//       vowel++;
+//     }else if(DIGITS.includes(char)){
+//          digit++;
+//     }else if(char === ' '){
+//         space++;
+//     }else{
+//         consonant++;
+//     }
+
+//   }
+
+//   console.log('vowel:', vowel);
+//   console.log('consonants:', consonant);
+//   console.log('digits:', digit);
+//   console.log('spaces:', space);
+
+
+
